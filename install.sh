59516131eedfff49308860dc7addb101a65ec887
Daniel Henninger <info@dhe.de>
2017-01-05 17:57:48 +0100

    Create install.sh
    Erster commit

---
+++ install.sh
@@ -0,0 +1,89 @@
+#!/usr/bin/env bash
+
+# Terminal
+# brew cask install iterm2
+
+# Editor
+brew cask install sublime-text
+brew cask install textwrangler
+brew cask install atom
+
+# Browser
+brew cask install google-chrome
+brew cask install firefox
+brew cask install opera
+
+# Entwicklungsumgebung und Tools
+#brew cask install zendserver
+#brew cask install zend-studio
+brew cask install phpstorm
+brew cask install google-web-designer
+brew cask install android-studio
+brew cask install navicat-premium
+brew cask install dash
+brew cask install alfred
+brew cask install transmit
+brew cask install livereload
+brew cask install imageoptim
+brew cask install codekit
+brew cask install soapui
+brew cask install postman
+brew cask install virtualbox
+brew cask install virtualbox-extension-pack
+brew cask install vagrant
+brew cask install tower
+
+# Kommunikation
+brew cask install x-lite
+brew cask install skype-for-business
+
+# Dateiverwaltung
+brew cask install path-finder
+
+# Passwortverwaltung
+brew cask install 1password
+
+# Google Tools
+brew cask install google-nik-collection
+brew cask install google-hangouts
+brew cask install google-cloud-sdk
+brew cask install google-adwords-editor
+
+# Cloud Speicher
+brew cask install google-drive
+brew cask install dropbox
+
+# VPN
+brew cask install tunnelblick
+
+# Aufgaben Management
+brew cask install omnifocus
+brew cask install omnifocus-clip-o-tron
+
+# Bildschirmaufzeichnung
+brew cask install camtasia
+
+# Fernwartung
+brew cask install teamviewer
+
+# Notizen
+brew cask install evernote
+
+# Smartphone oder Tablet auf Desktop sppiegeln
+brew cask install reflector
+
+# Video Player
+brew cask install vlc
+
+# Videos speichern z.B. von Youtube
+brew cask install clipgrab
+
+# Packprogramm
+brew cask install the-unarchiver
+
+# Musik
+brew cask install spotify
+brew cask install amazon-music
+
+# Office Programme
+brew cask install microsoft-office
